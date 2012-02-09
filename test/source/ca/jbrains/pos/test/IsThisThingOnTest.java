b6cba11128008b38027c09c34af8b8d95ca9b043
J. B. Rainsberger <me@jbrains.ca>
2012-02-09 09:24:28 +0100

    Starting point.

---
+++ test/source/ca/jbrains/pos/test/IsThisThingOnTest.java
@@ -0,0 +1,14 @@
+package ca.jbrains.pos.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IsThisThingOnTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
