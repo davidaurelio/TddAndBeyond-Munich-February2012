6ba73e165bec27c3fe0327be54aee1ab0d3a19f7
J. B. Rainsberger <me@jbrains.ca>
2012-02-10 09:22:30 +0100

    Fixed imports.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,10 +1,8 @@
 package ca.jbrains.pos.test;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.Test;
 
